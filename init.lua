e8cd7a0c91483d498063ad49aaa25043dc901793
Aliaksei Niamkovich <aliaksei.niamkovich@gmail.com>
2026-06-15 15:03:55 +0200

    feat: change theme to white rose pine

--- init.lua
+++ init.lua
@@ -48,6 +48,7 @@ vim.o.confirm = true
 -- Clear highlights on search when pressing <Esc> in normal mode
 --  See `:help hlsearch`
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
+vim.keymap.set('n', 'q', '<Nop>', { desc = 'Disable accidental macro recording' })
 
 -- Sync clipboard between OS and Neovim.
 --  Schedule the setting after `UiEnter` because it can increase startup-time.
@@ -725,26 +726,10 @@ require('lazy').setup({
     },
   },
 
-  { -- You can easily change to a different colorscheme.
-    -- Change the name of the colorscheme plugin below, and then
-    -- change the command in the config to whatever the name of that colorscheme is.
-    --
-    -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-    'folke/tokyonight.nvim',
-    priority = 1000, -- Make sure to load this before all the other start plugins.
-    config = function()
-      ---@diagnostic disable-next-line: missing-fields
-      require('tokyonight').setup {
-        styles = {
-          comments = { italic = false }, -- Disable italics in comments
-        },
-      }
-
-      -- Load the colorscheme here.
-      -- Like many other themes, this one has different styles, and you could load
-      -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'tokyonight-moon'
-    end,
+  {
+    'rose-pine/neovim',
+    name = 'rose-pine',
+    config = function() vim.cmd 'colorscheme rose-pine-dawn' end,
   },
 
   -- Highlight todo, notes, etc in comments
